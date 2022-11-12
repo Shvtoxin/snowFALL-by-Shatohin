49445c6655ad95d59e402b0df33ff44b6663bd91
Shvtoxin <Sasha.shvtoxin@mail.ru>
2022-11-12 16:22:13 +0300

    Добавьте файлы проекта.

---
+++ snowFALL/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace snowFALL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::snowFALL.Properties.Resources.back;
+            this.ClientSize = new System.Drawing.Size(1149, 655);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Snowfall by shatohin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ snowFALL/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snowFALL
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly IList<Snow> snowfall;
+        private readonly Timer timer;
+        private readonly Bitmap snowImg;
+        private readonly Bitmap backFon;
+        private readonly int speed = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            snowfall = new List<Snow>();
+            backFon = (Bitmap)Properties.Resources.back;
+            snowImg = (Bitmap)Properties.Resources.snow;
+            CreateSnow();
+            timer = new Timer
+            {
+                Interval = 1
+            };
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            DrawScen();
+            timer.Start();
+        }
+        private void CreateSnow()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 200; i++)
+            {
+                snowfall.Add(new Snow
+                {
+                    X = rnd.Next(Screen.PrimaryScreen.WorkingArea.Width),
+                    Y = -rnd.Next(Screen.PrimaryScreen.WorkingArea.Height),
+                    Size = rnd.Next(1, 30)
+                });
+            }
+        }
+        private void DrawScen()
+        {
+            var scene = new Bitmap(backFon,
+                 ClientRectangle.Width,
+                 ClientRectangle.Height);
+            for (var i = 0; i < snowfall.Count; i++)
+            {
+
+                if (snowfall[i].Y > Screen.PrimaryScreen.WorkingArea.Height)
+                {
+                    snowfall[i].Y = -snowfall[i].Size;
+                }
+                else
+                {
+                    snowfall[i].Y += speed + snowfall[i].Size;
+                }
+            }
+            for (var i = 0; i < snowfall.Count; i++)
+            {
+                if (snowfall[i].Y > 0)
+                {
+                    var gr = Graphics.FromImage(scene);
+                    gr.DrawImage(snowImg,
+                        new Rectangle(
+                        snowfall[i].X,
+                        snowfall[i].Y,
+                        snowfall[i].Size,
+                        snowfall[i].Size));
+                }
+            }
+            var a = CreateGraphics();
+            a.DrawImage(scene, 0, 0);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawScen();
+        }
+    }
+}
